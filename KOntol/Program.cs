55d87712e80b0d7c8fb2c47f1e5c9d67f1156d0f
vanyusha82228 <ivan00.02@mail.ru>
2022-12-03 15:58:05 +0300

    method for displaying an array on the screen

--- KOntol/Program.cs
+++ KOntol/Program.cs
@@ -9,3 +9,10 @@
     return arrayString;
 }
 
+void ShowArrayString(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
