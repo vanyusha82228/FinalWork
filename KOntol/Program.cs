70fbcb688d798ab129cff3ae6e2bdafdc5470012
vanyusha82228 <ivan00.02@mail.ru>
2022-12-03 15:50:42 +0300

    method to fill array

--- KOntol/Program.cs
+++ KOntol/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] FillingFrrayOfStrings(int size)
+{
+    string[] arrayString = new string[size];
+    for(int i=0;i<size;i++)
+    {
+        Console.Write("Enter the value of the array element: ");
+        arrayString[i]= Convert.ToString(Console.ReadLine());
+    }
+    return arrayString;
+}
+
